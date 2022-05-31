6250cbfdea142e95687b0a4b06b85d9a309bfba7
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-05-31 21:07:38 +0200

    Added new products

--- ProjektSemestralny/ProjektSemestralny.Logika/Data/ApplicationDBContext.cs
+++ ProjektSemestralny/ProjektSemestralny.Logika/Data/ApplicationDBContext.cs
@@ -24,25 +24,60 @@ namespace ProjektSemestralny.Logika.Data
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 10, Name = "Mobiles"},
                 new Category { Id = 11, Name = "Laptops"},
-                new Category { Id = 12, Name = "Monitors"}
+                new Category { Id = 12, Name = "Monitors"},
+                new Category { Id = 13, Name = "Smartwatches"},
+                new Category { Id = 14, Name = "Soundbars"},
+                new Category { Id = 15, Name = "Projectors" },
+                new Category { Id = 16, Name = "Powerbanks"},
+                new Category { Id = 17, Name = "Printers" },
+                new Category { Id = 18, Name = "Consoles"},
+                new Category { Id = 19, Name = "Refrigerators" },
+                new Category { Id = 20, Name = "Washing machines" },
+                new Category { Id = 21, Name = "Ovens" }
                 );
 
             modelBuilder.Entity<Producent>().HasData(
                 new Producent { Id = 50, Name = "Samsung" },
                 new Producent { Id = 51, Name = "Motorola" },
-                new Producent { Id = 52, Name = "Panasonic" }
+                new Producent { Id = 52, Name = "Panasonic" },
+                new Producent { Id = 53, Name = "Electrolux"},
+                new Producent { Id = 54, Name = "Sony"},
+                new Producent { Id = 55, Name = "HP"},
+                new Producent { Id = 56, Name = "Huawei"},
+                new Producent { Id = 57, Name = "Boose"},
+                new Producent { Id = 58, Name = "Sbs"},
+                new Producent { Id = 59, Name = "Epson"}
                 );
 
             modelBuilder.Entity<CategoryProducent>().HasData(
                 new CategoryProducent { Id = 1, CategoryId = 10, ProducentId = 50 },
                 new CategoryProducent { Id = 2, CategoryId = 11, ProducentId = 51 },
-                new CategoryProducent { Id = 3, CategoryId = 12, ProducentId = 52 }
+                new CategoryProducent { Id = 3, CategoryId = 12, ProducentId = 52 },
+                new CategoryProducent { Id = 4, CategoryId = 13, ProducentId = 50},
+                new CategoryProducent { Id = 5, CategoryId = 14, ProducentId = 57},
+                new CategoryProducent { Id = 6, CategoryId = 15, ProducentId = 59},
+                new CategoryProducent { Id = 7, CategoryId = 16, ProducentId = 58},
+                new CategoryProducent { Id = 8, CategoryId = 17, ProducentId = 59},
+                new CategoryProducent { Id = 9, CategoryId = 18, ProducentId = 54},
+                new CategoryProducent { Id = 10, CategoryId = 19, ProducentId = 50},
+                new CategoryProducent { Id = 11, CategoryId = 20, ProducentId = 53},
+                new CategoryProducent { Id = 12, CategoryId = 21, ProducentId = 53}
+
                 );
 
             modelBuilder.Entity<Item>().HasData(
                 new Item { Id = 1, Name = "Galaxy S22 Ultra", Description = "Samsung's fastest, most powerful chip ever. That means, a faster CPU and GPU compared to Galaxy S21 Ultra. It’s an epic leap for smartphone technology.", Price = 1199.00, CategoryProducentId = 1 },
                 new Item { Id = 2, Name = "ATRIX 4G", Description = "Laptop dock for the Motorola ATRIX 4G for a more interactive computer-like experience from your smartphone", Price = 500, CategoryProducentId= 2 },
-                new Item { Id = 3, Name = "TH-75CQ2U 75 4K UHD Professional TV", Description = "6 Models of Entry-level Displays with 4K High-Definition Image Quality.", CategoryProducentId = 3 }
+                new Item { Id = 3, Name = "TH-75CQ2U 75 4K UHD Professional TV", Description = "6 Models of Entry-level Displays with 4K High-Definition Image Quality.", Price = 600, CategoryProducentId = 3 },
+                new Item { Id = 4, Name = "Smartwatch SAMSUNG Galaxy Watch 4", Description = "Galaxy Watch4 Classic runs faster (compared to Galaxy Watch3), efficiently activates applications that you have even more space to do.", Price = 1199.00, CategoryProducentId = 4 },
+                new Item { Id = 5, Name = "Soundbar BOSE Smart Soundbar 900", Description = "Bose Voice4Video's unique technology extends voice control through Alex's assistant, something no other soundbar can do.", Price = 4699.00, CategoryProducentId = 5 },
+                new Item { Id = 6, Name = "Projektor EPSON EH-TW740", Description = "Enjoy your favorite movies with this Full HD projector.", Price = 2699.00, CategoryProducentId = 6 },
+                new Item { Id = 7, Name = "Powerbank SBS 20000mAh Czarny TTBB20000FASTK", Description = "With a high capacity battery, you don't have to worry about running out of battery power on your device.", Price = 119.00, CategoryProducentId = 7 },
+                new Item { Id = 8, Name = "EPSON EcoTank L3251", Description = "Save up to 90% on printing costs with this Epson EcoTank L3251 printer.", Price = 869.00, CategoryProducentId = 8 },
+                new Item { Id = 9, Name = "PlayStation 5 B Chassis", Description = "The PS5 takes gaming to a new level you don't even expect.", Price = 2799.00, CategoryProducentId = 9 },
+                new Item { Id = 10, Name = "PlayStation 5 B Chassis", Description = "The PS5 takes gaming to a new level you don't even expect.", Price = 2799.00, CategoryProducentId = 10 },
+                new Item { Id = 11, Name = "SAMSUNG Bespoke RB38A6B2E22/EF", Description = "Looking for a unique refrigerator that fits perfectly into your kitchen style? If you're looking at design, the BESPOKE range of refrigerators will allow you to choose from a range of colors and textures.", Price = 2949.00, CategoryProducentId = 11 },
+                new Item { Id = 12, Name = "ELECTROLUX EOC8H31Z", Description = "The combination of steam and hot air ensures that the dishes do not dry and can be cooked at a lower temperature.", Price = 2799.00, CategoryProducentId = 12 }
                 );
                 }
 
---
+++ ProjektSemestralny/ProjektSemestralny.Logika/Migrations/20220531190308_new_products.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjektSemestralny.Logika.Migrations
+{
+    public partial class new_products : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 13, "Smartwatches" },
+                    { 20, "Washing machines" },
+                    { 19, "Refrigerators" },
+                    { 18, "Consoles" },
+                    { 21, "Ovens" },
+                    { 16, "Powerbanks" },
+                    { 15, "Projectors" },
+                    { 14, "Soundbars" },
+                    { 17, "Printers" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 600.0);
+
+            migrationBuilder.InsertData(
+                table: "Producents",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 59, "Epson" },
+                    { 53, "Electrolux" },
+                    { 54, "Sony" },
+                    { 55, "HP" },
+                    { 56, "Huawei" },
+                    { 57, "Boose" },
+                    { 58, "Sbs" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoryProducents",
+                columns: new[] { "Id", "CategoryId", "ProducentId" },
+                values: new object[,]
+                {
+                    { 4, 13, 50 },
+                    { 10, 19, 50 },
+                    { 11, 20, 53 },
+                    { 12, 21, 53 },
+                    { 9, 18, 54 },
+                    { 5, 14, 57 },
+                    { 7, 16, 58 },
+                    { 6, 15, 59 },
+                    { 8, 17, 59 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "CategoryProducentId", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 4, 4, "Galaxy Watch4 Classic runs faster (compared to Galaxy Watch3), efficiently activates applications that you have even more space to do.", "Smartwatch SAMSUNG Galaxy Watch 4", 1199.0 },
+                    { 10, 10, "The PS5 takes gaming to a new level you don't even expect.", "PlayStation 5 B Chassis", 2799.0 },
+                    { 11, 11, "Looking for a unique refrigerator that fits perfectly into your kitchen style? If you're looking at design, the BESPOKE range of refrigerators will allow you to choose from a range of colors and textures.", "SAMSUNG Bespoke RB38A6B2E22/EF", 2949.0 },
+                    { 12, 12, "The combination of steam and hot air ensures that the dishes do not dry and can be cooked at a lower temperature.", "ELECTROLUX EOC8H31Z", 2799.0 },
+                    { 9, 9, "The PS5 takes gaming to a new level you don't even expect.", "PlayStation 5 B Chassis", 2799.0 },
+                    { 5, 5, "Bose Voice4Video's unique technology extends voice control through Alex's assistant, something no other soundbar can do.", "Soundbar BOSE Smart Soundbar 900", 4699.0 },
+                    { 7, 7, "With a high capacity battery, you don't have to worry about running out of battery power on your device.", "Powerbank SBS 20000mAh Czarny TTBB20000FASTK", 119.0 },
+                    { 6, 6, "Enjoy your favorite movies with this Full HD projector.", "Projektor EPSON EH-TW740", 2699.0 },
+                    { 8, 8, "Save up to 90% on printing costs with this Epson EcoTank L3251 printer.", "EPSON EcoTank L3251", 869.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 55);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 56);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProducents",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 53);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 54);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 57);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 58);
+
+            migrationBuilder.DeleteData(
+                table: "Producents",
+                keyColumn: "Id",
+                keyValue: 59);
+
+            migrationBuilder.UpdateData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 0.0);
+        }
+    }
+}
